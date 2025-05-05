ade121944f2111f36e68d0391bb0a31d9a852bb3
Kevin Christman Lumban Tobing <KEVINTOBINGGAMING@GMAIL.COM>
2025-05-05 23:28:09 +0700

    Delete server directory

--- server/routes/courseRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllCourse, getCourseId } from '../controllers/courseController.js';
-
-const courseRouter = express.Router();
-
-courseRouter.get('/all', getAllCourse);
-courseRouter.get('/:id', getCourseId);
-
-export default courseRouter;
